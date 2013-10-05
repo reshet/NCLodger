74fba5d807c021c20c9bbb57e8d30c1006d61453
Yaroslav <yaroslav.dmytruk@gmail.com>
2013-10-05 10:42:21 +0300

    from yaroslav

---
+++ src/main/java/Yaroslav.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Miredean
+ * Date: 05.10.13
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class Yaroslav {
+}
