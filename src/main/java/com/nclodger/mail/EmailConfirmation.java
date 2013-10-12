9d41db9f92ed3dce96831a216b3f0ff1f064e199
Iuliia <bieliaievays@gmail.com>
2013-10-12 13:01:57 +0300

    EmailConfirmation

---
+++ src/main/java/com/nclodger/mail/EmailConfirmation.java
@@ -0,0 +1,12 @@
+package com.nclodger.mail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Julia
+ * Date: 12.10.13
+ * Time: 12:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmailConfirmation {
+
+}
