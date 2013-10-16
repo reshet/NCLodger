4afa8d38f97f73d80ad007ce596a38d4726c611e
reshet <reshet.ukr@gmail.com>
2013-10-16 16:45:36 +0300

    css works, mail not yet. Database ready

--- src/main/java/com/nclodger/dao/UserDao.java
+++ src/main/java/com/nclodger/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.nclodger.dao;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
 import org.springframework.stereotype.Component;
 
 import javax.enterprise.context.spi.Context;
@@ -22,6 +23,19 @@ import java.sql.Statement;
 @Component("userdao")
 public class UserDao implements UserDaoInterface {
     private static Connection dataBase = null;
+    static{
+
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("jdbc/NCLodger");
+            dataBase = ds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (NamingException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
 
     public void insert(int _id, String _email, String _pswd, String _name, int register_confirm) {
         String sql = "INSERT INTO User(id,email,pswd,name,register_confirmed)" +
@@ -41,8 +55,41 @@ public class UserDao implements UserDaoInterface {
     }
 
     @Override
-    public void insert(Users _user) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void insert(Users user) {
+        //Tested valid sql
+        //INSERT INTO "Users" (id_user,username,email,pswd,user_type,is_blocked) values (0,'reshet','reshet.ukr@gmail.com','tratata','customer',0);
+
+
+        try {
+            PreparedStatement prep = dataBase.prepareStatement(
+                    "INSERT INTO Users (id_user, username,email,pswd,is_blocked) values (?,?,?,?,0);"
+            );
+            //NamedParameterStatement p = new NamedParameterStatement(con, query);
+            prep.setInt(1,10);
+            prep.setString(2,user.getName());
+            prep.setString(3,user.getEmail());
+            prep.setString(4,user.getPswd());
+            prep.setInt(5,0);
+            java.sql.ResultSet res = prep.executeQuery();
+            res.next();
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+       /* String sql = "INSERT INTO Users(id_user,username,email,pswd,is_blocked) " +
+                "values " +
+                "(" +  user.getId()+ "," + user.getName() + "," + user.getEmail() + "," + user.getPswd() + ",0);");
+       */
+        //Statement st = null;
+       /* try {
+            //st = dataBase.createStatement();
+            java.sql.ResultSet res = st.executeQuery(sql);
+            res.next();
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }*/
+        // TODO Prepared statement read
+
         int k = 0;
     }
 
