a4c3fa593a3772096c9eea08a4a266ad9720bead
Yaroslav <yaroslav.dmytruk@gmail.com>
2013-10-24 09:41:13 +0300

    Delete vitmin41k.java

--- src/main/java/vitmin41k.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Шуми
- * Date: 05.10.13
- * Time: 10:22
- * To change this template use File | Settings | File Templates.
- */
-public class vitmin41k {
-}
