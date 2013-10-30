dc6c9d415fdab24f284fb7703ffb6a8c93305f84
Fergonesh <pashashenhofer@gmail.com>
2013-10-30 18:22:27 +0200

    update SignUpAction;

---
+++ src/main/java/com/nclodger/control/action/ConfirmationAction.java
@@ -0,0 +1,11 @@
+package com.nclodger.control.action;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pasha
+ * Date: 10/30/13
+ * Time: 5:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfirmationAction {
+}
