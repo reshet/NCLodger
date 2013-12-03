f4d52f9e1afd798294230c00b6dd9cfb00c2ec5b
Fergonesh <pashashenhofer@gmail.com>
2013-12-03 02:07:04 +0200

    added check for confirmation in sign in;

--- src/main/java/com/nclodger/control/action/access/SignInAction.java
+++ src/main/java/com/nclodger/control/action/access/SignInAction.java
@@ -4,9 +4,6 @@ import com.nclodger.control.action.Action;
 import com.nclodger.dao.UserDao;
 import com.nclodger.domain.Users;
 import com.nclodger.myexception.MyException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,7 +17,6 @@ public class SignInAction extends Action {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws MyException {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("bean-config.xml");
         UserDao users = (UserDao) ctx.getBean("userDAO");
         if(!users.isExistEmail(request.getParameter("email"))){
             request.setAttribute("regConfirm",true);
@@ -36,6 +32,11 @@ public class SignInAction extends Action {
         Users user;
         try {
             user = users.getUserObj(request.getParameter("email"),request.getParameter("password"));
+            //check for confirmation to login
+            if(user.get_confirm_register()!=1){
+                request.setAttribute("regConfirm",true);
+                return "login";
+            }
             if (user != null && user.get_confirm_register() == 1){
                 request.getSession().setAttribute("email",user.getEmail());
                 request.getSession().setAttribute("username",user.getName());
