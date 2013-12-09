85db9fc519b89ff34de3edc676c5faaffca429a3
antoshka412 <antoshka412@gmail.com>
2013-12-09 15:48:34 +0200

    users in admin dashboard tab1 can be seen without clicking on tab

--- src/main/java/com/nclodger/control/action/admin/AdminDeleteUserAction.java
+++ src/main/java/com/nclodger/control/action/admin/AdminDeleteUserAction.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * To change this template use File | Settings | File Templates.
  */
 public class AdminDeleteUserAction  extends Action {
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         UserDao userDao = new UserDao();
